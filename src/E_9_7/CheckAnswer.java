ba2a07a819c582de4f2bfc796a942bb64aebef86
Kawsar Ahmmed <kawsarahmmed10001@gmail.com>
2024-05-31 11:54:25 +0600

    Check User response in every way to check to the correct answer in CheckAnswer.java class

---
+++ src/E_9_7/CheckAnswer.java
@@ -0,0 +1,33 @@
+package E_9_7;
+
+import java.util.Scanner;
+
+public class CheckAnswer extends Question3{
+
+    @Override
+    public boolean checkAnswer(String response) {
+        String answerInLowerCase = getAnswer().toLowerCase();
+        String responseInLowerCase = response.toLowerCase();
+        if (responseInLowerCase.equals(answerInLowerCase)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Question3 question3 = new CheckAnswer();
+
+        question3.setText("Who is the Inventor of Java?");
+        question3.setAnswer("James Gosling");
+        question3.display();
+
+        String response = in.nextLine();
+        if (question3.checkAnswer(response)) {
+            System.out.println("Correct Answer.");
+        } else{
+            System.out.println("The Inventor of Java was"+ " __" + question3.getAnswer() + "__");
+        }
+    }
+}
---
+++ src/E_9_7/Question3.java
@@ -0,0 +1,40 @@
+package E_9_7;
+
+public class Question3 {
+    private String text;
+    private String answer;
+
+    public Question3(String text, String answer) {
+        this.text = text;
+        this.answer = answer;
+    }
+
+    public Question3() {
+        text = "";
+        answer = "";
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean checkAnswer(String response) {
+        return response.equals(answer);
+    }
+
+    public void display() {
+        System.out.println(text);
+    }
+}
